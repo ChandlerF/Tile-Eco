b519d293463e1c48cbbb5d5913c362eaabe41ca5
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2021-08-11 02:42:24 -0500

    Trying to add mountain tile spawning

--- GMTK/Assets/SpawnSourceBlocks.cs
+++ GMTK/Assets/SpawnSourceBlocks.cs
@@ -18,6 +18,9 @@ public class SpawnSourceBlocks : MonoBehaviour
 
     [SerializeField] Grid Grid;
 
+    [SerializeField] GameObject Mountain;
+    private Vector3 MountainSpawn;
+
     void Start()
     {
         GetSpawnLocation();
@@ -28,15 +31,143 @@ public class SpawnSourceBlocks : MonoBehaviour
         SunSpawn = new Vector2(Random.Range(MinX, MaxX), Random.Range(MinY, MaxY));
         WaterSpawn = new Vector2(Random.Range(MinX, MaxX), Random.Range(MinY, MaxY));
 
-        if (Mathf.Abs(SunSpawn.x - WaterSpawn.x) > 1.3 && Mathf.Abs(SunSpawn.y - WaterSpawn.y) > 1.3)       //distance between spawns is greater than 1.3
+        if (Mathf.Abs(SunSpawn.x - WaterSpawn.x) > 4 && Mathf.Abs(SunSpawn.y - WaterSpawn.y) > 2)       //distance between spawns is greater than 1.3
         {
             Instantiate(SunSource, Grid.GetCellCenterWorld(Grid.WorldToCell(SunSpawn)), SunSource.transform.rotation);
 
             Instantiate(WaterSource, Grid.GetCellCenterWorld(Grid.WorldToCell(WaterSpawn)), WaterSource.transform.rotation);
+
+
+
+
+            //Mountain Spawn
+
+            //Keep it as an obsticle that spawns in the middle until there's functionality for the mountain
+            MountainSpawn = new Vector2((SunSpawn.x + WaterSpawn.x) / 2, (SunSpawn.y + WaterSpawn.y) / 2);
+
+            Instantiate(Mountain, Grid.GetCellCenterWorld(Grid.WorldToCell(MountainSpawn)), Mountain.transform.rotation);
+
+
+
+
+
+
+            int FirstRandomNumber = Random.Range(-1, 1);
+            int SecondRandomNumber = Random.Range(-1, 1);
+
+            Vector2 SecondMountainCoordinates;
+            Vector2 ThirdMountainCoordinates;
+
+            if (FirstRandomNumber == 0 && SecondRandomNumber == 0)
+            {
+                SecondMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x, (int)Grid.WorldToCell(MountainSpawn).y + 1);
+
+                ThirdMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x - 1, (int)Grid.WorldToCell(MountainSpawn).y);
+                Debug.Log("If statement called");
+            }
+            else
+            {
+
+                SecondMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x + FirstRandomNumber, (int)Grid.WorldToCell(MountainSpawn).y + SecondRandomNumber);                //MountainSpawn.x + FirstRandomNumber, MountainSpawn.y + SecondRandomNumber);
+
+
+                ThirdMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x - FirstRandomNumber, (int)Grid.WorldToCell(MountainSpawn).y - SecondRandomNumber);
+
+            }
+
+            Debug.Log("---------");
+
+
+            Debug.Log("FirstRandomNumber :" + FirstRandomNumber);
+            Debug.Log("SecondRandomNumber :" + SecondRandomNumber);
+
+
+            Debug.Log("Mountain 1 :" + Grid.WorldToCell(MountainSpawn));
+            Debug.Log("Mountain 2 :" + Grid.WorldToCell(SecondMountainCoordinates));
+            Debug.Log("Mountain 3 :" + Grid.WorldToCell(ThirdMountainCoordinates));
+
+
+            Debug.Log("FirstRandomNumber + Mountain 1 :" + Grid.WorldToCell(MountainSpawn).x + FirstRandomNumber + " = " + (Grid.WorldToCell(MountainSpawn).x + FirstRandomNumber));
+            Debug.Log("SecondRandomNumber + Mountain 1 :" + Grid.WorldToCell(MountainSpawn).y + SecondRandomNumber + " = " + (Grid.WorldToCell(MountainSpawn).y + SecondRandomNumber));
+
+            Debug.Log("FirstRandomNumber + Mountain 2 :" + Grid.WorldToCell(SecondMountainCoordinates).x + FirstRandomNumber);
+            Debug.Log("SecondRandomNumber + Mountain 2 :" + Grid.WorldToCell(SecondMountainCoordinates).y + SecondRandomNumber);
+
+
+            Debug.Log("---------");
+
+
+
+            Instantiate(Mountain, Grid.GetCellCenterWorld(Grid.WorldToCell(SecondMountainCoordinates)), Mountain.transform.rotation);
+
+            Instantiate(Mountain, Grid.GetCellCenterWorld(Grid.WorldToCell(ThirdMountainCoordinates)), Mountain.transform.rotation);
+
+            /*Debug.Log(FirstRandomNumber);
+            Debug.Log(SecondRandomNumber);*/
         }
+
+
         else 
         {
-            GetSpawnLocation();
+            GetSpawnLocation();     //Inefficient, should place 1 spawn then base the other off that -------------------------------------------------
         }
     }
 }
+
+
+
+
+/*
+  
+            int FirstRandomNumber = Random.Range(-1, 1);
+            int SecondRandomNumber = Random.Range(-1, 1);
+
+            Vector2 SecondMountainCoordinates;
+            Vector2 ThirdMountainCoordinates;
+
+            if (FirstRandomNumber == 0 && SecondRandomNumber == 0)
+            {
+                SecondMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x, (int)Grid.WorldToCell(MountainSpawn).y + 1);
+
+                ThirdMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x - 1, (int)Grid.WorldToCell(MountainSpawn).y);
+                Debug.Log("If statement called");
+            }
+            else
+            {
+
+                SecondMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x + FirstRandomNumber, (int)Grid.WorldToCell(MountainSpawn).y + SecondRandomNumber);                //MountainSpawn.x + FirstRandomNumber, MountainSpawn.y + SecondRandomNumber);
+
+
+                ThirdMountainCoordinates = new Vector2((int)Grid.WorldToCell(MountainSpawn).x - FirstRandomNumber, (int)Grid.WorldToCell(MountainSpawn).y - SecondRandomNumber);
+
+            }
+
+            Debug.Log("---------");
+
+
+            Debug.Log("FirstRandomNumber :" + FirstRandomNumber);
+            Debug.Log("SecondRandomNumber :" + SecondRandomNumber);
+
+
+            Debug.Log("Mountain 1 :" + Grid.WorldToCell(MountainSpawn));
+            Debug.Log("Mountain 2 :" + Grid.WorldToCell(SecondMountainCoordinates));
+            Debug.Log("Mountain 3 :" + Grid.WorldToCell(ThirdMountainCoordinates));
+
+
+            Debug.Log("FirstRandomNumber + Mountain 1 :" + Grid.WorldToCell(MountainSpawn).x + FirstRandomNumber + " = " + (Grid.WorldToCell(MountainSpawn).x + FirstRandomNumber));
+            Debug.Log("SecondRandomNumber + Mountain 1 :" + Grid.WorldToCell(MountainSpawn).y + SecondRandomNumber + " = " + (Grid.WorldToCell(MountainSpawn).y + SecondRandomNumber));
+
+            Debug.Log("FirstRandomNumber + Mountain 2 :" + Grid.WorldToCell(SecondMountainCoordinates).x + FirstRandomNumber);
+            Debug.Log("SecondRandomNumber + Mountain 2 :" + Grid.WorldToCell(SecondMountainCoordinates).y + SecondRandomNumber);
+
+
+            Debug.Log("---------");
+
+
+
+            Instantiate(Mountain, Grid.GetCellCenterWorld(Grid.WorldToCell(SecondMountainCoordinates)), Mountain.transform.rotation);
+
+            Instantiate(Mountain, Grid.GetCellCenterWorld(Grid.WorldToCell(ThirdMountainCoordinates)), Mountain.transform.rotation);
+
+            /*Debug.Log(FirstRandomNumber);
+            Debug.Log(SecondRandomNumber);*/
